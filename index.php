7b2067b0a408e3dd0eb1bde4080997ae9b54695a
nagata-ryosuke <114294761+nagata-ryosuke@users.noreply.github.com>
2022-10-02 13:29:14 +0900

    3章チェックテスト
    FizzBuzz問題

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- FizzBuzz問題 3章チェックテスト -->
+
+<?php
+
+for ($i=1;$i<=100;$i++) {
+    if (($i % 3) == 0 && ($i % 5) == 0) {
+        echo 'FizzBuzz!';
+    } elseif (($i % 5) == 0) {
+        echo 'Buzz!';
+    } elseif (($i % 3) == 0) {
+        echo 'Fizz!';
+    } elseif (($i % 3) != 0 && ($i % 5) != 0) {
+        echo $i;
+    }
+    echo '<br>';
+}
